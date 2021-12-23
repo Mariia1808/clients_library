b4b5d3369cac6e16dd97b8cb2f72a5fe535cf9ed
Maria1808 <mvbannikova1808@gmail.com>
2021-12-23 22:25:39 +0500

    1

--- src/http/userAPI.js
+++ src/http/userAPI.js
@@ -2,19 +2,19 @@ import {$authHost, $host} from "./index";
 import jwt_decode from "jwt-decode";
 
 export const registration = async (name, email, password) => {
-    const {data} = await $host.post('api/user/registration', {name, email, password})
+    const {data} = await $host.post('https://arcane-everglades-82036.herokuapp.com/api/user/registration', {name, email, password})
     localStorage.setItem('token', data.token)
     return jwt_decode(data.token)
 }
 
 export const login = async (email, password) => {
-    const {data} = await $host.post('api/user/login', {email, password})
+    const {data} = await $host.post('https://arcane-everglades-82036.herokuapp.com/api/user/login', {email, password})
     localStorage.setItem('token', data.token)
     return jwt_decode(data.token)
 }
 
 export const check = async () => {
-    const {data} = await $authHost.get('api/user/auth' )
+    const {data} = await $authHost.get('https://arcane-everglades-82036.herokuapp.com/api/user/auth' )
     localStorage.setItem('token', data.token)
     return jwt_decode(data.token)
 }
