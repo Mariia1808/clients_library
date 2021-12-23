65945cf5d94a34eeb43053f3441772a39c39975c
Maria1808 <mvbannikova1808@gmail.com>
2021-12-23 23:56:01 +0500

    1

--- src/App.js
+++ src/App.js
@@ -4,10 +4,26 @@ import React from 'react';
 import {BrowserRouter} from "react-router-dom";
 import AppRouter from "./components/AppRouter";
 import NavBar from "./components/NavBar";
-
+import {Context} from "./index";
+import {check} from "./http/userAPI";
+import {Spinner} from "react-bootstrap";
 
 const App = observer(() => {
-  
+  const App = observer(() =>{
+    const {user} =useContext(Context)
+    const [loading, setLoading] =useState(true)
+// alert("тута")
+    useEffect(()=>{
+            check().then(data =>{
+                user.setUser(data)
+                user.setIsAuth(true)
+            }).finally(()=> setLoading(false))
+    }, [])
+    
+    if(loading)
+    {
+        return <Spinner animation={"grow"}/>
+    }
     return (
       <BrowserRouter>
           <div>
